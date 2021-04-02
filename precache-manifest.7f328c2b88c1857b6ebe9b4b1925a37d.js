10db92529f84b46d2ceb4d8f39d9833396f32f2a
John William Davis <jwdavisdeveloper@gmail.com>
2021-04-02 00:47:41 -0700

    Updates

---
+++ precache-manifest.7f328c2b88c1857b6ebe9b4b1925a37d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cae5ffd74aa321cf1c02",
+    "url": "/course-directory/static/css/main.1c026df4.chunk.css"
+  },
+  {
+    "revision": "cae5ffd74aa321cf1c02",
+    "url": "/course-directory/static/js/main.cae5ffd7.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "815979c4589c03a44bc682d805764c19",
+    "url": "/course-directory/index.html"
+  }
+];
